89c7c7190294cf1ffc2f9404a29eb2b3abc07339
sumangal44 <sumangalkaran44@gmail.com>
2026-09-16 14:01:33 +0530

    Add word embedding demonstration using Word2Vec

---
+++ pg6.py
@@ -0,0 +1,19 @@
+# demonstrate word embedding using word2vec
+from gensim.models import Word2Vec
+
+sentences = [
+    ["The", 'cat', 'is', 'sitting', 'on', 'the', 'mat'],
+    ["The", 'dog', 'is', 'playing', 'with', 'the', 'ball'],
+    ["The", 'bird', 'is', 'flying', 'in', 'the', 'sky'],
+    ["The", 'fish', 'is', 'swimming', 'in', 'the', 'water'],
+    ["The", 'child', 'is', 'playing', 'in', 'the', 'park']
+]
+model = Word2Vec(sentences,vector_size=50, window=2, min_count=1,workers=2)
+print("original text", sentences)
+print("word embedding cat", model.wv['cat'])
+
+print("word similar to cat", model.wv.most_similar('cat',topn=3)) 
+
+
+
+
